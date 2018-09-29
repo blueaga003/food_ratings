4a4de49c7dfa15178e57358b7f4b886d91d36162
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-29 12:17:44 -0700

    Add print_ratings() function

--- ratings.py
+++ ratings.py
@@ -7,6 +7,8 @@ import sys
 import_file = open(sys.argv[1], "r")
 
 def create_sorted_dictionary_of_restaurant_ratings(rated_restaurants):
+    """ Creates sorted dictionary of restaurant ratings. """
+
     restaurant_ratings_by_name = {}
 
     for line in rated_restaurants:
@@ -18,7 +20,14 @@ def create_sorted_dictionary_of_restaurant_ratings(rated_restaurants):
         # print(restaurant_ratings_by_name)
 
     restaurant_ratings_by_name_sorted = sorted(restaurant_ratings_by_name.items())
-    print(restaurant_ratings_by_name_sorted)
+    return(restaurant_ratings_by_name_sorted)
     # print(restaurant_ratings_by_name.items())
 
-create_sorted_dictionary_of_restaurant_ratings(import_file)
+
+sorted_ratings = create_sorted_dictionary_of_restaurant_ratings(import_file)
+
+def print_ratings(sorted_dictionary):
+    for line in sorted_dictionary:
+        print("{} is rated at {}".format(line[0], line[1]))
+
+(print_ratings(sorted_ratings))
