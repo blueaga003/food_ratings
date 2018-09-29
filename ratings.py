2208b86e8ea9a4317aa15028de77e5ce79673690
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-29 12:10:40 -0700

    Add function for creating sorted dictionary

--- ratings.py
+++ ratings.py
@@ -6,13 +6,19 @@ import sys
 
 import_file = open(sys.argv[1], "r")
 
-restaurant_ratings_by_name = {}
+def create_sorted_dictionary_of_restaurant_ratings(rated_restaurants):
+    restaurant_ratings_by_name = {}
 
-for line in import_file:
-    line = line.strip().split(":")
-    print(line)
-    dictionary_key = line[0]
-    dictionary_value = line[1]
-    restaurant_ratings_by_name[dictionary_key] = dictionary_value
-    print(restaurant_ratings_by_name)
-    
+    for line in rated_restaurants:
+        line = line.strip().split(":")
+        # print(line)
+        dictionary_key = line[0]
+        dictionary_value = line[1]
+        restaurant_ratings_by_name[dictionary_key] = dictionary_value
+        # print(restaurant_ratings_by_name)
+
+    restaurant_ratings_by_name_sorted = sorted(restaurant_ratings_by_name.items())
+    print(restaurant_ratings_by_name_sorted)
+    # print(restaurant_ratings_by_name.items())
+
+create_sorted_dictionary_of_restaurant_ratings(import_file)
