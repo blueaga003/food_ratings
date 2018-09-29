e3f86980a0c91813904e9e97e0c4a026dbc6a3de
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-29 12:59:23 -0700

    append user dictionary to source dictionary

--- ratings.py
+++ ratings.py
@@ -6,18 +6,25 @@ import sys
 
 import_file = open(sys.argv[1], "r")
 
-def create_sorted_dictionary_of_restaurant_ratings(rated_restaurants):
+def create_sorted_dictionary_of_restaurant_ratings(rated_restaurants_from_source, rated_restaurants_by_users):
     """ Creates sorted dictionary of restaurant ratings. """
 
     restaurant_ratings_by_name = {}
 
-    for line in rated_restaurants:
+    for line in rated_restaurants_from_source:
         name, rating = line.strip().split(":")
+        restaurant_ratings_by_name[name] = rating
+
+    for entry in rated_restaurants_by_users:
+        # line = " ".join(line)
+        # line = line.strip().split(",")
+        # print(line)
+        name = entry[0]
+        rating = entry[1]
         restaurant_ratings_by_name[name] = rating
 
     return sorted(restaurant_ratings_by_name.items())
 
-sorted_ratings = create_sorted_dictionary_of_restaurant_ratings(import_file)
 
 def print_ratings(sorted_dictionary):
     """ Print out sorted ratings dictionary. """
@@ -25,13 +32,21 @@ def print_ratings(sorted_dictionary):
     for line in sorted_dictionary:
         print("{} is rated at {}".format(line[0], line[1]))
 
-(print_ratings(sorted_ratings))
-
 def ask_user_restaurant_rating():
+    user_restaurant_ratings_list = []
+    print("Follow the prompts below, when you are finished enter 'quit' into 'Name of restaurant' prompt.")
     while True:
-        print("Follow the prompts below, when you are finished enter 'quit' into 'Name of restaurant' prompt.")
         restaurant_name = input("Name of restaurant: ").title()
         if restaurant_name == "Quit":
             break
         restaurant_rating = input("{}'s rating: ".format(restaurant_name))
-ask_user_restaurant_rating()
+        user_restaurant_ratings_list.append((restaurant_name, restaurant_rating))
+    return user_restaurant_ratings_list
+    #print(" ".join(user_restaurant_ratings_list))
+    #print(user_restaurant_ratings_list)    
+
+
+user_input_dictionary = ask_user_restaurant_rating()
+sorted_ratings = create_sorted_dictionary_of_restaurant_ratings(import_file, user_input_dictionary)
+
+(print_ratings(sorted_ratings))
